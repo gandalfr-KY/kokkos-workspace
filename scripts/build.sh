144b3e00343bcf622ddf5cbe06e973031c30c283
gandalfr-KY <oukanimiso@gmail.com>
2024-07-26 11:49:18 +0000

    modify architecture

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ ! -d "kokkos" ]; then
-    git clone https://github.com/kokkos/kokkos.git
-fi
 mkdir -p build && cd build
 
 if [ "$1" == "--gpu" ] || [ "$1" == "-g" ]; then
     cmake .. -DKokkos_ENABLE_OPENMP=ON \
     -DKokkos_ENABLE_CUDA=ON \
-    -DKokkos_ARCH_AMPERE80=ON \
+    -DKokkos_ARCH_AMPERE86=ON \
     -DCMAKE_BUILD_TYPE=Release
 else
     cmake .. -DKokkos_ENABLE_OPENMP=ON -DCMAKE_BUILD_TYPE=Release
